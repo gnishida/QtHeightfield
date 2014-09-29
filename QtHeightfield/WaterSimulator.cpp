0757627af3cf2da253269eb8e58b571963ffb9b3
gnishida <gnishida@purdue.edu>
2014-09-29 18:09:07 -0400

    Update parameter values.

--- QtHeightfield/WaterSimulator.cpp
+++ QtHeightfield/WaterSimulator.cpp
@@ -69,8 +69,8 @@ void WaterSimulator::draw(float t)
     // Set the uniform variables
     glUniform1i(_texUnitLoc, 0);
 	float center[] = {-0.2, -0.2, 0.3, 0.2, -0.1, 0.4};
-	float frequency[] = {50.0, 40.0, 25.0};
-	float amplitude[] = {0.01, 0.01, 0.01};
+	float frequency[] = {200.0, 160.0, 100.0};
+	float amplitude[] = {0.0025, 0.0025, 0.0025};
 	float lightDir[] = {0.0, 0.0, 1.0};
 	float island[] = {0.5, 0.5};
 	glUniform2fv(_centerLoc, 3, center);
